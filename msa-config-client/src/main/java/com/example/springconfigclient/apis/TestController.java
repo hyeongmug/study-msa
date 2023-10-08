c6ae5d74aebe69aa7721a1121611b7f29731ccb8
lim <skygudanr@naver.com>
2023-10-08 14:53:47 +0900

    스프링 클라우드 클라이언트

---
+++ msa-config-client/src/main/java/com/example/springconfigclient/apis/TestController.java
@@ -0,0 +1,21 @@
+package com.example.springconfigclient.apis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RefreshScope
+public class TestController {
+
+    @Value("${test}")
+    private String test;
+
+    @GetMapping("/test")
+    public String test() {
+        return test;
+    }
+}
