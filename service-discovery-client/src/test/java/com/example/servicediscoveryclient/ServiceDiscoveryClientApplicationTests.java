5a3edd02664c88a23c4b154c0a7ca5fa88d0ddac
lim <skygudanr@naver.com>
2023-10-12 15:36:29 +0900

    서비스 디스커버리

---
+++ service-discovery-client/src/test/java/com/example/servicediscoveryclient/ServiceDiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicediscoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
